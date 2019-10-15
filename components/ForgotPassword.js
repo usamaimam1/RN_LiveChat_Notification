1f9272957f60bd0a48038b49ba2c5814953c8f59
SystemPlusGlobal <systemplusglobal@MacBooks-MacBook-Pro.local>
2019-10-15 15:39:38 +0500

    Generic design ForgotPassword ios = android

--- components/ForgotPassword.js
+++ components/ForgotPassword.js
@@ -14,6 +14,7 @@ import {
     Alert
 } from 'react-native'
 import firebase from 'react-native-firebase'
+import { SafeAreaView } from 'react-navigation'
 
 export default class ForgotPassword extends React.Component {
     static navigationOptions = {
@@ -27,15 +28,15 @@ export default class ForgotPassword extends React.Component {
             passwordChangeCode: this.props.navigation.state.params.code,
             newPassword: null,
             confirmnewPassword: null,
-            receivedCode: this.props.navigation.state.params.code ? true:false
+            receivedCode: this.props.navigation.state.params.code ? true : false
         }
         this.handleUserEmailChange = this.handleUserEmailChange.bind(this)
         this.handleUserPasswordChange = this.handleUserPasswordChange.bind(this)
         this.handleUserPasswordChangeConfirm = this.handleUserPasswordChangeConfirm.bind(this)
         this.handlepasswordChangeCode = this.handlepasswordChangeCode.bind(this)
     }
-    componentDidMount(){
-        
+    componentDidMount() {
+
     }
     handleUserEmailChange(userEmail) {
         this.setState({ userEmail: userEmail })
@@ -50,93 +51,103 @@ export default class ForgotPassword extends React.Component {
         this.setState({ passwordChangeCode: code })
     }
     render() {
-        const {navigate} = this.props.navigation
-        return (
-            <ScrollView style={styles.Container} keyboardShouldPersistTaps="always">
-                <ImageBackground style={styles.background} source={require('../assets/bg-hd.jpg')}>
-                    {/* <ActivityIndicator size="large" animating={this.state.showActivity} /> */}
-                    <View style={{ flex: 1 }}>
-                        <View style={styles.logo} >
-                            <Image source={require('../assets/ReactNativeFirebase.png')} style={{ width: Dimensions.get("window").width - 20, margin: 10, flex: 1 }} resizeMode="contain" >
+        const { navigate } = this.props.navigation
+        const ContentToRender = (
+            <ImageBackground style={styles.background} source={require('../assets/splash-bg.jpg')}>
+                {/* <ActivityIndicator size="large" animating={this.state.showActivity} /> */}
+                <View style={{ flex: 1 }}>
+                    <View style={styles.logo} >
+                        <Image source={require('../assets/ReactNativeFirebase.png')} style={{ width: Dimensions.get("window").width - 20, margin: 10, flex: 1 }} resizeMode="contain" >
 
-                            </Image>
-                        </View>
+                        </Image>
                     </View>
-                    <View style={styles.form} >
-                        <View style={styles.inputContainer}>
-                            {this.state.receivedCode ? null:
+                </View>
+                <View style={styles.form} >
+                    <View style={styles.inputContainer}>
+                        {this.state.receivedCode ? null :
                             <TextInput placeholder="Enter Email : " textContentType="emailAddress" style={styles.Text} onChangeText={this.handleUserEmailChange} value={this.state.userEmail} >
 
                             </TextInput>
-                            }
-                            {this.state.receivedCode ?
-                                <TextInput
-                                    placeholder="Enter Password : "
-                                    textContentType="password"
-                                    style={styles.Text}
-                                    onChangeText={this.handleUserPasswordChange}
-                                    secureTextEntry={true}
-                                    value={this.state.newPassword}>
+                        }
+                        {this.state.receivedCode ?
+                            <TextInput
+                                placeholder="Enter Password : "
+                                textContentType="password"
+                                style={styles.Text}
+                                onChangeText={this.handleUserPasswordChange}
+                                secureTextEntry={true}
+                                value={this.state.newPassword}>
 
-                                </TextInput>
-                            :null}
-                            {this.state.receivedCode ? 
-                                <TextInput 
-                                    placeholder="Enter Password Again : " 
-                                    textContentType="password" 
-                                    style={styles.Text} 
-                                    onChangeText={this.handleUserPasswordChangeConfirm} 
-                                    secureTextEntry={true} 
-                                    value={this.state.confirmnewPassword}>
-                                </TextInput>
-                            :null}
-                        </View>
-                        <View style={styles.signinButton}>
-                            <Button 
-                                title={"Submit"} 
-                                onPress = {()=>{
-                                    if(!this.state.receivedCode){
-                                        firebase.auth().sendPasswordResetEmail(this.state.userEmail,{android:{packageName:"com.spl.firebasetest"} ,iOS:{},url:"https://firebasetestrandom.page.link", handleCodeInApp:true})
-                                        .then(()=>{
+                            </TextInput>
+                            : null}
+                        {this.state.receivedCode ?
+                            <TextInput
+                                placeholder="Enter Password Again : "
+                                textContentType="password"
+                                style={styles.Text}
+                                onChangeText={this.handleUserPasswordChangeConfirm}
+                                secureTextEntry={true}
+                                value={this.state.confirmnewPassword}>
+                            </TextInput>
+                            : null}
+                    </View>
+                    <View style={styles.signinButton}>
+                        <Button
+                            title={"Submit"}
+                            onPress={() => {
+                                if (!this.state.receivedCode) {
+                                    firebase.auth().sendPasswordResetEmail(this.state.userEmail, { android: { packageName: "com.spl.firebasetest" }, iOS: {}, url: "https://firebasetestrandom.page.link", handleCodeInApp: true })
+                                        .then(() => {
                                             // console.log(url)
                                             Alert.alert(
                                                 'Notification',
                                                 'Please Check Your Email',
                                                 [
-                                                  {text: 'OK', onPress: () => navigate('Home')}
+                                                    { text: 'OK', onPress: () => navigate('Home') }
                                                 ],
-                                                {cancelable: false},
-                                              );
+                                                { cancelable: false },
+                                            );
 
                                             // this.setState({receivedCode:true})
 
                                         })
-                                        .catch(err=>{
-                                            ToastAndroid.show(err.message,ToastAndroid.LONG)
+                                        .catch(err => {
+                                            ToastAndroid.show(err.message, ToastAndroid.LONG)
                                         })
-                                    }else{
-                                        if(this.state.newPassword && this.state.confirmnewPassword){
-                                            if(this.state.newPassword == this.state.confirmnewPassword){
-                                                firebase.auth().confirmPasswordReset(this.state.passwordChangeCode,this.state.newPassword).then(()=>{
-                                                    ToastAndroid.show("Password Changed Successfully..",ToastAndroid.SHORT)
-                                                    this.setState({newPassword:null,confirmPasswordReset:null,passwordChangeCode:null})
-                                                    navigate('Home')
-                                                }).catch(err=>{
-                                                    ToastAndroid.show(err.message,ToastAndroid.SHORT)
-                                                })
-                                            }else{
-                                                ToastAndroid.show("Passwords Do not match",ToastAndroid.SHORT)
-                                                this.setState({newPassword:null,confirmnewPassword:null})
-                                            }
-                                        }else{
-                                            ToastAndroid.show('Please do not leave the fields empty',ToastAndroid.SHORT)
+                                } else {
+                                    if (this.state.newPassword && this.state.confirmnewPassword) {
+                                        if (this.state.newPassword == this.state.confirmnewPassword) {
+                                            firebase.auth().confirmPasswordReset(this.state.passwordChangeCode, this.state.newPassword).then(() => {
+                                                ToastAndroid.show("Password Changed Successfully..", ToastAndroid.SHORT)
+                                                this.setState({ newPassword: null, confirmPasswordReset: null, passwordChangeCode: null })
+                                                navigate('Home')
+                                            }).catch(err => {
+                                                ToastAndroid.show(err.message, ToastAndroid.SHORT)
+                                            })
+                                        } else {
+                                            ToastAndroid.show("Passwords Do not match", ToastAndroid.SHORT)
+                                            this.setState({ newPassword: null, confirmnewPassword: null })
                                         }
+                                    } else {
+                                        ToastAndroid.show('Please do not leave the fields empty', ToastAndroid.SHORT)
                                     }
-                                }}/>
-                        </View>
+                                }
+                            }} />
                     </View>
-                </ImageBackground>
-            </ScrollView>)
+                </View>
+            </ImageBackground>)
+        if (Platform.OS === "ios") {
+            return (
+                <SafeAreaView style={styles.Container}>
+                    {ContentToRender}
+                </SafeAreaView>)
+        } else {
+            return (
+                <ScrollView style={styles.Container} keyboardShouldPersistTaps="always">
+                    {ContentToRender}
+                </ScrollView>
+            )
+        }
     }
 }
 const styles = StyleSheet.create({
