72c691bb71ebc38b10ad79ce3a8a721442be1054
Muhammad Usama Imam <usamaimam1@outlook.com>
2020-01-16 20:31:21 +0000

    Dashboard Structure fixed and Projects Redux Added

--- redux/actions/actionTypes.js
+++ redux/actions/actionTypes.js
@@ -1,2 +1,9 @@
 export const ADD_USER = 'ADD_USER'
 export const SET_USER = 'SET_USER'
+export const PRINT_USER = 'PRINT_USER'
+
+export const ADD_PROJECT = 'ADD_PROJECT'
+export const SET_PROJECT = 'SET_PROJECT'
+export const DELETE_PROJECT = 'DELETE_PROJECT'
+export const ADD_PROJECTS = 'ADD_PROJECTS'
+export const PRINT_PROJECTS = 'PRINT_PROJECTS'
--- redux/store.js
+++ redux/store.js
@@ -1,4 +1,5 @@
 import rootReducer from './reducers/index'
 import {createStore} from 'redux'
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 export default store
